42dee99cc2edf9a49a12dcb16878d474bbbeb59e
FelipeWanner <felipe.w.pakua@gmail.com>
2024-08-10 20:34:59 +1000

    adding the footer component

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -2,23 +2,75 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: 'Arial', sans-serif;
+    font-family: Arial, sans-serif;
   }
 
-  h1, h2, h3, h4, h5, h6 {
-    margin: 0;
-    padding: 0;
+  .text-small {
+    font-size: 13px;
+  }
+
+  .divs-facilities {
+    width: 325px;
+  }
+
+  .color-black {
+    background-color: #000000;
+  }
+
+  .text-color-black {
+    color: #000000;
+  }
+
+  .color-purple {
+    background-color: #9353FF;
+  }
+
+  .text-color-purple {
+    color: #9353FF;
+  }
+
+  .color-veryLightGreen {
+    background-color: #DAFF01;
+  }
+
+  .text-color-veryLightGreen {
+    color: #DAFF01;
+  }
+
+  .color-lightGrey {
+    background-color: #cccccc;
+  }
+
+  .text-color-lightGrey {
+    color: grey;
+  }
+
+  .white-circle-icon {
+    width: 40px;
+    height: 40px;
+    background-color: white;
+  }
+
+  .my-ruler {
+    width: 100%; 
+    text-align: center; 
+    border-bottom: 1px solid grey; 
+    line-height: 0.1em;
+    margin: 10px 0 20px; 
+  }
+   
+  .my-ruler span { 
+    background:#fff; 
+    padding:0 10px; 
+  }
+
+  .custom-border {
+    border-bottom: 1px solid black;
   }
 
   a {
-    text-decoration: none;
-    color: inherit;
+    cursor: pointer;
   }
-
-  /* Add any other global styles here */
 `;
 
 export default GlobalStyle;
