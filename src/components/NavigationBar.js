ba4211ea7bb4cf5a84c032eb26e7ef05be2254d0
FelipeWanner <felipe.w.pakua@gmail.com>
2024-08-10 20:02:20 +1000

    adding the Navbar component

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,55 +1,59 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import meteoraLogo from '../assets/logo-meteora.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-// Styled-component for custom styles
 const StyledNavbar = styled(Navbar)`
-  background-color: #000000; // Black background
-  .navbar-brand img {
-    height: 40px; // Adjust logo size
-  }
-  .nav-link {
-    color: #ffffff !important; // White text
-    &:hover {
-      color: #9353FF !important; // Purple on hover
-    }
-  }
+  background-color: #000000;
 `;
 
 const NavigationBar = () => {
   return (
-    <StyledNavbar expand="lg" variant="dark">
-      <Container>
+    <StyledNavbar expand="md" variant="dark">
+      <div className="container-fluid">
         <Navbar.Brand as={Link} to="/">
           <img
-            src="/assets/logo-meteora.png"
+            src={meteoraLogo}
             alt="Meteora Logo"
+            height="40"
+            className="d-inline-block align-top"
           />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/our-stores">Our Stores</Nav.Link>
-            <Nav.Link as={Link} to="/whats-new">What's New</Nav.Link>
+        <Navbar.Toggle aria-controls="navbarSupportedContent" />
+        <Navbar.Collapse id="navbarSupportedContent">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/" className="nav-link active">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/our-stores" className="nav-link">
+              Our Stores
+            </Nav.Link>
+            <Nav.Link as={Link} to="/whats-new" className="nav-link">
+              What's New
+            </Nav.Link>
           </Nav>
-          <form className="d-flex ms-auto">
-            <input
-              className="form-control me-2 rounded-0"
+          <Form className="d-flex">
+            <FormControl
               type="search"
               placeholder="Search for a product"
+              className="me-2 rounded-0"
               aria-label="Search"
             />
-            <button
-              className="btn btn-outline-light rounded-0"
-              type="submit"
+            <Button
+              variant="outline-light"
+              className="rounded-0"
+              onClick={() =>
+                window.location.href =
+                  'https://yv45m72jfaycdkfq7becpqtflm0xinkh.lambda-url.ap-southeast-2.on.aws/'
+              }
             >
               Search
-            </button>
-          </form>
+            </Button>
+          </Form>
         </Navbar.Collapse>
-      </Container>
+      </div>
     </StyledNavbar>
   );
 };
