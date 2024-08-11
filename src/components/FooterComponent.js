f5b7f4c1ede0f5c8ad1bc3891e19620339db4567
FelipeWanner <felipe.w.pakua@gmail.com>
2024-08-11 15:36:29 +1000

    transfering responsibility of the gap between footer and content to the footerComponent.js

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -4,6 +4,10 @@ import FacilitiesSection from './FacilitiesSection';
 import NewsletterForm from './NewsletterForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const MainContent = styled.div`
+  padding-top: 3rem;  
+`; //cria espaço entre o topo do footer e o conteudo da pagina
+
 const FooterContainer = styled.footer`
   background-color: #000000;
   color: #ffffff;
@@ -13,13 +17,13 @@ const FooterContainer = styled.footer`
 
 const Footer = () => {
   return (
-    <>  
+    <MainContent>  
       <FacilitiesSection />
       <NewsletterForm />
       <FooterContainer>
         <p>2023 &copy; Felipe Rafael Wanner / Fictional Meteora Store project</p>
       </FooterContainer>
-    </>
+    </MainContent>
 
   );
 };
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,22 +2,16 @@ import React from 'react';
 import Carousel from '../components/Carousel';
 import CategoryCardsGrid from '../components/CategoryCardsGrid';
 import FeaturedProductsGrid from '../components/FeaturedProductsGrid';
-import styled from 'styled-components';
-
-const MainContent = styled.div`
-  padding-bottom: 3rem;  
-`; //cria espaço entre o grid e o footer
-
 
 const HomePage = () => {
   return (
-    <MainContent>
+    <>
       <Carousel />
       <h2 className="text-center my-3 my-xl-5">Search for categories:</h2>
       <CategoryCardsGrid />
       <h2 className="text-center my-3 my-xl-5">Products everybody is looking for!</h2>
       <FeaturedProductsGrid />
-    </MainContent>
+    </>
   );
 };
 
